c773542aae415a4d44343094682ac33351249380
Jiraphon Yenphraphai <jyenphraphai@gmail.com>
2023-02-04 14:03:43 -0500

    day 73

--- hw1/nyuc/argmanip.c
+++ hw1/nyuc/argmanip.c
@@ -1,3 +1,8 @@
+/*
+I don't understand variadic function that is provided in the c documentation. 
+So, I read it from this website https://www.geeksforgeeks.org/variadic-functions-in-c/ and realize that
+we can access the next argument by calling va_arg and point the pointer to it. 
+*/
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,12 +12,10 @@
 char **manipulate_args(int argc, const char *const *argv, int (*const manip)(int)){
   char **args;
   int stringLength;
-  args = (char**)malloc((argc+1) * sizeof(char*));
-  
+  args = (char **)malloc((argc+1) * sizeof(char *));
   for(int i = 0; i < argc; i++, argv++) {
-   
     stringLength = strlen(*argv);
-    args[i] = (char*)malloc((stringLength+1) * sizeof(char));
+    args[i] = (char *)malloc((stringLength+1) * sizeof(char));
     strcpy(args[i], *argv);
     //printf("%s", args[i]);
     for (int j=0; j<stringLength; j++)
@@ -20,38 +23,34 @@ char **manipulate_args(int argc, const char *const *argv, int (*const manip)(int
     args[i][stringLength] = '\0';
     //printf("%s", args[i]);
     //exit(0);
-   
   }
   args[argc] = NULL;
   return args;
 }
-/*
-void free_copied_args(char **upper_args, char **lower_args){
-  char **temp = upper_args;
-  while (*upper_args++)
-    free(*upper_args);
-  free(temp);
 
-  temp = lower_args;
-  while (*lower_args++)
-    free(*lower_args);
-  free(temp);
+void helper(char **args);
+void free_copied_args(char **fmt, ...){
+  va_list args;
+  va_start(args, fmt);
+  while (fmt){
+    helper(fmt);
+    fmt = va_arg(args, char **);
+  }
+
+  va_end(args);
 }
-*/
-void free_copied_args(char **upper_args, char **lower_args){
+
+void helper(char **args){
   int length = 0;
-  char **tmp=upper_args;
+  char **tmp=args;
   while (*tmp++)
     length++;
   
-  for(int i = 0; i <= length; i++){
-    free(upper_args[i]);
-    free(lower_args[i]);
-  }
-  free(upper_args);
-  free(lower_args);
+  for(int i = 0; i <= length; i++)
+    free(args[i]);
+    
+  free(args);
 }
-
 
 
 
--- hw1/nyuc/nyuc.c
+++ hw1/nyuc/nyuc.c
@@ -6,11 +6,11 @@
 int main(int argc, const char *const *argv) {
   char **upper_args = manipulate_args(argc, argv, toupper);
   char **lower_args = manipulate_args(argc, argv, tolower);
-
+  
   for (char *const *p = upper_args, *const *q = lower_args; *p && *q; ++argv, ++p, ++q) {
     printf("[%s] -> [%s] [%s]\n", *argv, *p, *q);
   }
 
-  //free_copied_args(upper_args, lower_args, NULL);
-  free_copied_args(upper_args, lower_args);
+  free_copied_args(upper_args, lower_args, NULL);
+  //free_copied_args(upper_args, lower_args);
 }
