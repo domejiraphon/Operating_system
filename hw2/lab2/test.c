962593052b19c0e0524b40e021d53e3983dc4f81
Jiraphon Yenphraphai <dome@Jiraphons-MacBook-Pro.local>
2023-03-07 11:02:05 -0500

    remove etc

--- hw2/lab2/test.c
+++ hw2/lab2/test.c
@@ -1,15 +1,62 @@
-// C program to implement cond(), signal()
-// and wait() functions
 #include <pthread.h>
 #include <stdio.h>
-#include <unistd.h>
 
-int main()
-{
-	printf("Helllo \n");
-  fflush(stdout);
-    int ret = fork();
-  printf("123 \n");
+#define NUM_THREADS 2
 
-	return 0;
+int counter = 0;
+pthread_spinlock_t lock;
+
+void *thread_func(void *threadid) {
+  long tid;
+  tid = (long)threadid;
+  printf("Thread %ld starting...\n", tid);
+  
+  // acquire the spinlock
+  pthread_spin_lock(&lock);
+  
+  // simulate a long-running task
+  sleep(5);
+  
+  // increment the counter
+  counter++;
+  
+  // release the spinlock
+  pthread_spin_unlock(&lock);
+  
+  printf("Thread %ld exiting...\n", tid);
+  pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+  pthread_t threads[NUM_THREADS];
+  int rc;
+  long t;
+  
+  // initialize the spinlock
+  pthread_spin_init(&lock, 0);
+
+  // create the threads
+  for (t = 0; t < NUM_THREADS; t++) {
+    rc = pthread_create(&threads[t], NULL, thread_func, (void *)t);
+    if (rc) {
+      printf("ERROR; return code from pthread_create() is %d\n", rc);
+      return -1;
+    }
+  }
+
+  // join the threads
+  for (t = 0; t < NUM_THREADS; t++) {
+    rc = pthread_join(threads[t], NULL);
+    if (rc) {
+      printf("ERROR; return code from pthread_join() is %d\n", rc);
+      return -1;
+    }
+  }
+
+  // destroy the spinlock
+  pthread_spin_destroy(&lock);
+
+  printf("Final counter value: %d\n", counter);
+
+  return 0;
 }
